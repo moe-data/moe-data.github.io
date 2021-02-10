1a1ea399795cfc117809b46648da96094dfdda0f
conbanwa <35380552+conbanwa@users.noreply.github.com>
2021-02-10 10:42:59 +0800

    Update app.js

--- dist/quest/app.js
+++ dist/quest/app.js
@@ -15183,9 +15183,9 @@ app=({
   }
 })
 app.onLaunch()
-z(app.set('ahistory'),[23,323,{name:3323}])
+app.set('ahistory',[23,323,{name:3323}])
 z(app.get('ahistory'))
 z($.cookie('ahistory'))
-z($.cookie('ihistory'),[23,323,{name:3323}])
+$.cookie('ihistory',[23,323,{name:3323}])
 z($.cookie('ihistory'))
 z(app.get('ihistory'))
