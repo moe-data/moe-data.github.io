cf4b0c911cf89c0330bfb33f5e5be4a9a8651aad
conbanwa <35380552+conbanwa@users.noreply.github.com>
2021-02-10 10:22:21 +0800

    Update wx.js

--- dist/quest/wx.js
+++ dist/quest/wx.js
@@ -2,9 +2,9 @@ function func(){}
 const wx={
     clearStorage: func,
     getStorageSync:function(key){
-       return $.cookie(key)
+       return JSON.parse($.cookie(key))
     },
     setStorageSync:function(key,value){
-        $.cookie(key,value, { expires: 999, path: '/' })
+        $.cookie(key,JSON.stringify(value), { expires: 999, path: '/' })
     }
 }
