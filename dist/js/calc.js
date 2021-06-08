1e11c3aed46563c3a0fa04135726e908d7a04c7b
conbanwa <j6j7@qq.com>
2021-06-08 10:17:56 +0800

    fmt

--- dist/js/calc.js
+++ dist/js/calc.js
@@ -25,14 +25,14 @@ const ranget = GetRequest("t", 1);
 let jsindex = 0;
 var bigdata = [];
 var slotitem;
-const api_=(q == 'd') ? 'api_' : '';
-const jsonfile= (q == 'd') ? 'api_mst_slotitem' : 'cstype';
-var lseg = { i: "", fuel:"",  ammo:"",  steel:"",  bauxite:"", 資材:"", secretary:"", denominator: '', times: '', ratio: '' };
+const api_ = (q == 'd') ? 'api_' : '';
+const jsonfile = (q == 'd') ? 'api_mst_slotitem' : 'cstype';
+var lseg = { i: "", fuel: "", ammo: "", steel: "", bauxite: "", 資材: "", secretary: "", denominator: '', times: '', ratio: '' };
 var kj = false;
 var ship;
 var csjson = [];
 var bar = 1;
-$('.loading').css("width",Width())
+$('.loading').css("width", Width())
 $.getJSON("parsed/cstype.json").done(function (result) {
     csjson = result;
     jsonover();
@@ -40,15 +40,15 @@ $.getJSON("parsed/cstype.json").done(function (result) {
 $.getJSON("parsed/" + jsonfile + ".json").done(function (result) {
     slotitem = result;
     jsonover();
-    var oname=[];
-    for(let j=0;j<o.length;j++){
+    var oname = [];
+    for (let j = 0; j < o.length; j++) {
         oname.push(addemoji(formatOnlyname(o[j])))
     }
-    document.title = oname.length?oname:"全部"+(q=='d'?"装备":"舰娘")
+    document.title = oname.length ? oname : "全部" + (q == 'd' ? "装备" : "舰娘")
 });
 $('#denominator').val(Number(GetRequest("a", 1)));
 var sorted = [];
-var minlv = GetRequest("l")=='true'?true:false;
+var minlv = GetRequest("l") == 'true' ? true : false;
 function progress(p) {
     bar += p;
     $('.progress-bar').css("width", bar + '%');
@@ -66,97 +66,99 @@ if (!havelang) {
         }
     }
 }
-function jsonover(){    
+function jsonover() {
     jsindex++;
-    console.log(jsindex ,ranget.length + 2,bigdata.length);
+    console.log(jsindex, ranget.length + 2, bigdata.length);
     progress(1 / (ranget.length + 2) * 80);
     // if (jsindex == ranget.length + 1)$('h3.panel-title')[0].innerHTML = ("数据计算中。。。");
     if (jsindex >= ranget.length + 2) {
-    progress(20);
-    var filted = filt(group2By(bigdata, "i", "s"), o);
-    if (!filted.length) {
-        $('h3.panel-title')[0].innerHTML = ('无匹配的结果，请减少主查询个数，或将主查询改为副查询')
-    } else {
-        // console.log(filted.length);
-        o = o.concat(e);
-        isonl = isonladd(filted, o);
-        isonl.forEach(function(e) {
-            denominator = 0;
-            for (keys in e) {
-                if (keys[0] == 'n') {
-                    denominator += e[keys];
-                }
-            }
-            e['denominator'] = denominator;
-            e['ratio'] = 0;
-            e['times'] = 0;
-            for (key in e) {
-                if (key[0] == 'n') {
-                    k = key.slice(1, key.length);
-                    e[formatOnlyname(k)] = e[key] / e['denominator'];
-                    if (o.indexOf(k) > -1) {
-                        e['ratio'] += e[formatOnlyname(k)];
-                        e['times'] += e['n' + k];
+        progress(20);
+        var filted = filt(group2By(bigdata, "i", "s"), o);
+        if (!filted.length) {
+            $('h3.panel-title')[0].innerHTML = ('无匹配的结果，请减少主查询个数，或将主查询改为副查询')
+        } else {
+            // console.log(filted.length);
+            o = o.concat(e);
+            isonl = isonladd(filted, o);
+            isonl.forEach(function (e) {
+                denominator = 0;
+                for (keys in e) {
+                    if (keys[0] == 'n') {
+                        denominator += e[keys];
                     }
                 }
-            }
-        });
-        setdeno(1);
+                e['denominator'] = denominator;
+                e['ratio'] = 0;
+                e['times'] = 0;
+                for (key in e) {
+                    if (key[0] == 'n') {
+                        k = key.slice(1, key.length);
+                        e[formatOnlyname(k)] = e[key] / e['denominator'];
+                        if (o.indexOf(k) > -1) {
+                            e['ratio'] += e[formatOnlyname(k)];
+                            e['times'] += e['n' + k];
+                        }
+                    }
+                }
+            });
+            setdeno(1);
+        }
+        $('.loading').hide();
+    } else if (jsindex == 3) {
+        // var filted = filt(group2By(bigdata, "i", "s"), o);
+        //     if (filted.length){
+        //         o = o.concat(e);
+        //         isonl = isonladd(filted, o);
+        //         isonl.forEach(function(e) {
+        //             denominator = 0;
+        //             for (keys in e) {
+        //                 if (keys[0] == 'n') {
+        //                     denominator += e[keys];
+        //                 }
+        //             }
+        //             e['denominator'] = denominator;
+        //             e['ratio'] = 0;
+        //             e['times'] = 0;
+        //             for (key in e) {
+        //                 if (key[0] == 'n') {
+        //                     k = key.slice(1, key.length);
+        //                     e[formatOnlyname(k)] = e[key] / e['denominator'];
+        //                     if (o.indexOf(k) > -1) {
+        //                         e['ratio'] += e[formatOnlyname(k)];
+        //                         e['times'] += e['n' + k];
+        //                     }
+        //                 }
+        //             }
+        //         });
+        //         setdeno();
+        // }
     }
-    $('.loading').hide();
-}else if(jsindex == 3) {
-// var filted = filt(group2By(bigdata, "i", "s"), o);
-//     if (filted.length){
-//         o = o.concat(e);
-//         isonl = isonladd(filted, o);
-//         isonl.forEach(function(e) {
-//             denominator = 0;
-//             for (keys in e) {
-//                 if (keys[0] == 'n') {
-//                     denominator += e[keys];
-//                 }
-//             }
-//             e['denominator'] = denominator;
-//             e['ratio'] = 0;
-//             e['times'] = 0;
-//             for (key in e) {
-//                 if (key[0] == 'n') {
-//                     k = key.slice(1, key.length);
-//                     e[formatOnlyname(k)] = e[key] / e['denominator'];
-//                     if (o.indexOf(k) > -1) {
-//                         e['ratio'] += e[formatOnlyname(k)];
-//                         e['times'] += e['n' + k];
-//                     }
-//                 }
-//             }
-//         });
-//         setdeno();
-// }
-}}
+}
 function setdeno(a) {
-    $(".deno").css("background","");
+    $(".deno").css("background", "");
     sorted = [];
-    isonl.forEach(function(e) {
+    isonl.forEach(function (e) {
         if (e['denominator'] > $('#denominator').val()) {
             sorted.push(e);
         };
     });
     if (sorted.length) {
         sortkey = GetRequest("s");
-        if(!sortkey==0){
+        if (!sortkey == 0) {
             sorted.sort(sortby);
             sorted.reverse();
         }
         langchange();
-        if(a){
-        progress(97);
-        $('div.panel').hide();
-        $("#foot").load("foot.html");}
+        if (a) {
+            progress(97);
+            $('div.panel').hide();
+            $("#foot").load("foot.html");
+        }
     } else {
         $('div.panel').show();
         $('h3.panel-title')[0].innerHTML = ('存在查询结果，但公式次数设置过大，请在左上方重新设置');
-        $(".deno").css("background","gold");
-            }
+        $(".deno").css("background", "gold");
+    }
 }
 var colorFlag = 0;
 $(document).ready(function () {
@@ -178,7 +180,7 @@ $(document).ready(function () {
         // $("#foot").load("foot.html");
         $('.search-input').bind("input onchange", function () {
             setTimeout(function () {
-                for (let i = 0; i <7; i++) {
+                for (let i = 0; i < 7; i++) {
                     document.getElementsByClassName('search-input')[i].value = Japanized(document.getElementsByClassName('search-input')[i].value);
                 }
             }, 50);
@@ -204,25 +206,26 @@ function thead(eg) {
             colspan = 1;
             rowspan = 2;
             formatter = formatLink;
-        }else{if(thls(e)){
+        } else if (thls(e)) {
             colspan = 1;
             rowspan = 2;
-            if(e=='資材'){
-            temp=devpic;
-            if(q=='d')visible=false;}
-        }}
+            if (e == '資材') {
+                temp = devpic;
+                if (q == 'd') visible = false;
+            }
+        }
         if (e == 'ratio') {
             colspan = 2;
             temp = '主查询合计';
             formatter = formatR;
         }
-        if (e == 'ratio' || thls(e)||e[0]=='n') {
+        if (e == 'ratio' || thls(e) || e[0] == 'n') {
             if (!isNaN(e[e.length - 1])) {
                 showid = e.slice(1, e.length);
-                temp = (q == 'd') ? formatItemId(showid):formatshipId(showid);
+                temp = (q == 'd') ? formatItemId(showid) : formatshipId(showid);
             }
             co.push({
-                title: getname(temp,kj),
+                title: getname(temp, kj),
                 field: e,
                 sortable: true,
                 filterControl: 'input',
@@ -240,10 +243,10 @@ function thead(eg) {
         var formatter = undefined;
         var f = '出货';
         if (e[0] == 'n') {
-            f = e.slice(1, e.length);
+            f = e.slice(1);
         }
         // if (e == 'ratio'||( e!='i'&& e!='fuel'&& e!='denominator'&& e!='times'&& e!='ammo'&& e[0]!='n'&& e[0]!='l')) {
-        if (e == 'ratio'||( !thls(e)&& e!='times'&& e[0]!='n'&& e[0]!='l')) {
+        if (e == 'ratio' || (!thls(e) && e != 'times' && e[0] != 'n' && e[0] != 'l')) {
             f = '%';
             if (e == 'ratio') {
                 f = '确率';
@@ -253,7 +256,7 @@ function thead(eg) {
         if (e[0] == 'l') {
             f = 'Lv';
         }
-        if (e == 'times' || e[0] == 'n' ||!thls(e)){
+        if (e == 'times' || e[0] == 'n' || !thls(e)) {
             col.push({
                 title: f,
                 field: e,
@@ -261,32 +264,33 @@ function thead(eg) {
                 formatter: formatter,
             })
         }
+        z(e, f)
     }
     return [co, col];
 }
-function thls(e){
-    return ['i','fuel','ammo','steel','bauxite','資材','secretary','denominator'].indexOf(e)>-1
+function thls(e) {
+    return ['i', 'fuel', 'ammo', 'steel', 'bauxite', '資材', 'secretary', 'denominator'].indexOf(e) > -1
 }
 function formatR(value) {
     return value ? (value * 100).toFixed(2) + '%' : null;
 }
 function formatLink(value, row, index) {
-    arrsplice(value,1);
-    var piedata=[];
-    for(key in row){
-        if(key[0]=='n'){
-            var itemID = key.slice(1,key.length);
-            var name=addemoji(formatOnlyname(itemID));
-            piedata.push({value: row[key], name: name});
+    arrsplice(value, 1);
+    var piedata = [];
+    for (key in row) {
+        if (key[0] == 'n') {
+            var itemID = key.slice(1, key.length);
+            var name = addemoji(formatOnlyname(itemID));
+            piedata.push({ value: row[key], name: name });
         }
-      }
+    }
     piedata.push(value);
-    return "<a href='javascript:pie(" +JSON.stringify(piedata)+")'>"+value+"</a>";
+    return "<a href='javascript:pie(" + JSON.stringify(piedata) + ")'>" + value + "</a>";
 }
-function pie(e){
+function pie(e) {
     // alert(JSON.stringify(e));
-    $.cookie('pie',JSON.stringify(e), { expires: 365, path: '/' });
-    window.location.href="pie.html";
+    $.cookie('pie', JSON.stringify(e), { expires: 365, path: '/' });
+    window.location.href = "pie.html";
 }
 function group2By(array, i, s) {
     let groups = {};
@@ -295,19 +299,19 @@ function group2By(array, i, s) {
         groups[group] = groups[group] || [];
         groups[group].push(o);
     });
-    let arr=[];
-    for (key in groups){
+    let arr = [];
+    for (key in groups) {
         arr.push(groups[key]);
     }
     return arr;
 }
 function filt(r, o) {
     var array = [];
-    r.forEach(function(element) {
+    r.forEach(function (element) {
         var f = true;
-        o.forEach(function(oe) {
+        o.forEach(function (oe) {
             var ff = false;
-            element.forEach(function(el) {
+            element.forEach(function (el) {
                 // f = f && el['o'] == o[k]
                 if (el['o'] == oe) {
                     ff = true;
@@ -325,25 +329,27 @@ function isonladd(array, o) {
     for (k = 0; k < o.length; k++) {
         egnrl(o[k]);
     }
-    if(q=='d'){
-    egnrl('-1');}
+    if (q == 'd') {
+        egnrl('-1');
+    }
     let groups = [];
     // o.push(-1)
     for (let i = 0, l = array.length; i < l; i++) {
         let is = array[i][0]['i'];
-        if(q=='d'){
-            if(typeof is=='string')is=JSON.parse(is);
+        if (q == 'd') {
+            if (typeof is == 'string') is = JSON.parse(is);
             is.push(1);
-            is.push(formatStype(array[i][0]['s']));}else{
+            is.push(formatStype(array[i][0]['s']));
+        } else {
             is.push(formatshipId(array[i][0]['s']));
         }
         var onal = { 'i': (is) };
-        for(let j= 0; j < array[i].length; j++) {
+        for (let j = 0; j < array[i].length; j++) {
             var item = jsonstr(array[i][j]['o']);
             egnrl(item);
             if (onal['n' + item]) {
                 onal['n' + item] += Number(array[i][j]['n']);
-            } else {onal['n' + item] = 0 + Number(array[i][j]['n']) };
+            } else { onal['n' + item] = 0 + Number(array[i][j]['n']) };
             if (minlv) {
                 if (onal['l' + item]) {
                     onal['l' + item] = Math.min(onal['l' + item], Number(array[i][j]['l']));
@@ -363,22 +369,22 @@ function egnrl(item) {
 }
 function initTable(lval) {
     if (lval) { kj = (lval.slice(0, 2) == 'zh' || lval.slice(0, 2) == 'ja') } else { console.log(lval) };
-    lsegname={'ratio':""};
-    for(key in lseg){
-        if(key[0]=='r'&&key!='ratio'){
-            key = key.slice(1,key.length);
-            var k=formatOnlyname(key);
-            lsegname[k]=0;
+    lsegname = { 'ratio': "" };
+    for (key in lseg) {
+        if (key[0] == 'r' && key != 'ratio') {
+            key = key.slice(1, key.length);
+            var k = formatOnlyname(key);
+            lsegname[k] = 0;
         }
     }
     loadprot(lseg);
     // console.log("load table", sorted)
-    addcol(sorted,"fuel","i",0);
-    addcol(sorted,"ammo","i",1);
-    addcol(sorted,"steel","i",2);
-    addcol(sorted,"bauxite","i",3);
-    addcol(sorted,"資材","i",4);
-    addcol(sorted,"secretary","i",5);
+    addcol(sorted, "fuel", "i", 0);
+    addcol(sorted, "ammo", "i", 1);
+    addcol(sorted, "steel", "i", 2);
+    addcol(sorted, "bauxite", "i", 3);
+    addcol(sorted, "資材", "i", 4);
+    addcol(sorted, "secretary", "i", 5);
     $('#mytab').bootstrapTable('destroy').bootstrapTable({
         method: 'get',
         data: sorted,
@@ -403,30 +409,30 @@ function initTable(lval) {
     document.getElementById("csscontainer").innerHTML = (".fix-sticky {padding-top:" + padding + "0px;position: fixed !important;overflow: hidden;  z-index: 100;}");
 }
 function formatOnlyname(value) {
-    var str=value;
-    if(value==-1){
+    var str = value;
+    if (value == -1) {
         return fail;
     };
-    for (nitem=0;nitem<slotitem.length;nitem++){
-        if(slotitem[nitem][api_+'id']==value){
-            str=slotitem[nitem][api_+'name'];
-    return bra(str,1);
+    for (nitem = 0; nitem < slotitem.length; nitem++) {
+        if (slotitem[nitem][api_ + 'id'] == value) {
+            str = slotitem[nitem][api_ + 'name'];
+            return bra(str, 1);
         }
-    }return bra(str,1);
-    }
-function addicon(name,c){
-    var str=bra(name,0);
-    var color=c?'<span style="color:'+c+';">▐</span>':'';//▮∎■
-    for (let a=0;a<slotitem.length;a++){
-        if(slotitem[a]['api_name']==str){
-    return '<span class="flex">'+color+itag(slotitem[a]['api_type'][3])+str+'</sapn>';
+    } return bra(str, 1);
+}
+function addicon(name, c) {
+    var str = bra(name, 0);
+    var color = c ? '<span style="color:' + c + ';">▐</span>' : '';//▮∎■
+    for (let a = 0; a < slotitem.length; a++) {
+        if (slotitem[a]['api_name'] == str) {
+            return '<span class="flex">' + color + itag(slotitem[a]['api_type'][3]) + str + '</sapn>';
         }
-    }return '<span class="flex">'+color+str+'</sapn>';
-    }
+    } return '<span class="flex">' + color + str + '</sapn>';
+}
 
-if(q=='c'){
+if (q == 'c') {
     // $('span.msg').css("width","76px");
-    document.getElementById('css').innerHTML+='span.msg{width:76px}' 
-}else{
-    document.getElementById('css').innerHTML+='span.msg{width:162px}' 
+    document.getElementById('css').innerHTML += 'span.msg{width:76px}'
+} else {
+    document.getElementById('css').innerHTML += 'span.msg{width:162px}'
 }
